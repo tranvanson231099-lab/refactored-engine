c3fc46db82a1a46d50539a6120419e6604696e69
Sơn <tranvanson231099@gmail.com>
2026-04-19 23:20:20 +0000

    Tiện ích này bao gồm tệp khóa '/media/fuse/crostini_6ca387c82353d4dc4760

--- background.js
+++ background.js
@@ -1,21 +1,23 @@
 
 // VietFlex Engine 2.1.6 - Background Processor
+// Chạy trong môi trường Service Worker, tuân thủ Manifest V3
+
 chrome.input.ime.onFocus.addListener((context) => {
   console.log('VietFlex: IME Focused', context);
 });
 
 chrome.input.ime.onKeyEvent.addListener((engineID, keyData) => {
-  if (keyData.type === 'keydown') {
-    // Logic xử lý Telex sẽ được tích hợp tại đây
-    return false;
-  }
+  // Logic xử lý Telex sẽ được gọi từ đây
   return false;
 });
 
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(() => {
   chrome.runtime.openOptionsPage();
 });
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    chrome.runtime.openOptionsPage();
+  }
   console.log('VietFlex Engine 2.1.6 đã được cài đặt thành công.');
 });
