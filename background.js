3c36b31de03119292139966c5dc3d44d764fa65d
Sơn <tranvanson231099@gmail.com>
2026-04-19 23:05:03 +0000

    sao khong co cho mo giao dien bang dieu khien

--- background.js
+++ background.js
@@ -6,7 +6,17 @@ chrome.input.ime.onFocus.addListener((context) => {
 
 chrome.input.ime.onKeyEvent.addListener((engineID, keyData) => {
   if (keyData.type === 'keydown') {
+    // Logic xử lý Telex sẽ được tích hợp tại đây cho bàn phím vật lý
     return false;
   }
   return false;
+});
+
+// Mở bảng điều khiển khi click vào icon nếu không có popup
+chrome.action.onClicked.addListener((tab) => {
+  chrome.runtime.openOptionsPage();
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('VietFlex Engine 2.1.6 đã được cài đặt thành công.');
 });
--- public/background.js
+++ public/background.js
@@ -4,15 +4,19 @@ chrome.input.ime.onFocus.addListener((context) => {
   console.log('VietFlex: IME Focused', context);
 });
 
-// Lắng nghe sự kiện click vào icon tiện ích để mở Bảng điều khiển
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({ url: 'index.html' });
-});
-
 chrome.input.ime.onKeyEvent.addListener((engineID, keyData) => {
   if (keyData.type === 'keydown') {
-    // Xử lý Telex Core sẽ được thực thi tại đây
+    // Logic xử lý Telex sẽ được tích hợp tại đây cho bàn phím vật lý
     return false;
   }
   return false;
+});
+
+// Mở bảng điều khiển khi click vào icon nếu không có popup
+chrome.action.onClicked.addListener((tab) => {
+  chrome.runtime.openOptionsPage();
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('VietFlex Engine 2.1.6 đã được cài đặt thành công.');
 });
